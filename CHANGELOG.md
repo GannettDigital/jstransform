f351d57749330a1d4a170dc9d6a68e97fed96020
Timothy Kuhlman <tim@backgroundprocess.com>
2018-03-30 08:49:03 -0600

    PENG-1204 - Expand the walk function to walk by instance or raw JSON

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # jstransform CHANGELOG
 
+# 0.0.3
+Tim Kuhlman - Expand the walk function to walk by instance or raw JSON
+
 # 0.0.2
 Tim Kuhlman - split out transform code from jsonschema
 
--- jsonschema/walk_test.go
+++ jsonschema/walk_test.go
@@ -7,15 +7,289 @@ import (
 )
 
 type testWalker struct {
-	calls map[string]json.RawMessage
+	calls    map[string]Instance
+	rawCalls map[string]json.RawMessage
 }
 
-func (tw *testWalker) walkFn(path string, value json.RawMessage) error {
-	tw.calls[path] = value
+func (tw *testWalker) walkFn(path string, i Instance, value json.RawMessage) error {
+	tw.calls[path] = i
+	tw.rawCalls[path] = value
 	return nil
 }
 
 func TestWalkJSONSchema(t *testing.T) {
+	tests := []struct {
+		description string
+		oneOfType   string
+		schemaPath  string
+		want        map[string]Instance
+		wantErr     bool
+	}{
+		{
+			description: "Basic walk, no allOf, no oneOf",
+			schemaPath:  "./test_data/image.json",
+			want: map[string]Instance{
+				"$.type": {Type: "string"},
+				"$.crops": {Type: "array", Items: []byte(`{
+        "type": "object",
+        "properties": {
+          "name": {
+            "type": "string",
+            "default": "name"
+          },
+          "width": {
+            "type": "number"
+          },
+          "height": {
+            "type": "number"
+          },
+          "path": {
+            "type": "string"
+          },
+          "relativePath": {
+            "type": "string"
+          }
+        },
+        "required":[
+          "name",
+          "width",
+          "height",
+          "path",
+          "relativePath"
+        ]
+			}`)},
+				"$.crops[*]": {Type: "object", Properties: map[string]json.RawMessage{
+					"name":         []byte(`{"type": "string", "default": "name"}`),
+					"width":        []byte(`{"type": "number" }`),
+					"height":       []byte(`{"type": "number" }`),
+					"path":         []byte(`{"type": "string" }`),
+					"relativePath": []byte(`{"type": "string" }`),
+				},
+					Required: []string{"name", "width", "height", "path", "relativePath"},
+				},
+				"$.crops[*].name":         {Type: "string"},
+				"$.crops[*].width":        {Type: "number"},
+				"$.crops[*].height":       {Type: "number"},
+				"$.crops[*].path":         {Type: "string"},
+				"$.crops[*].relativePath": {Type: "string"},
+				"$.URL": {Type: "object", Properties: map[string]json.RawMessage{
+					"publish": []byte(`{"type": "string",
+          "transform": {
+            "cumulo": {
+              "from" : [
+                {
+                  "jsonPath": "$.publishUrl"
+                }
+              ]
+            }
+          }}`),
+					"absolute": []byte(`{
+          "type": "string",
+          "transform": {
+            "cumulo": {
+              "from" : [
+                {
+                  "jsonPath": "$.absoluteUrl"
+                }
+              ]
+            }
+          }}`),
+				},
+					Required: []string{"publish", "absolute"},
+				},
+				"$.URL.publish":  {Type: "string"},
+				"$.URL.absolute": {Type: "string"},
+			},
+		},
+		{
+			description: "Walk with allOf, no oneOf",
+			schemaPath:  "./test_data/embed_parent.json",
+			want:        map[string]Instance{"$.type": {Type: "string"}},
+		},
+		{
+			description: "Walk with oneOf, no allOf",
+			oneOfType:   "image",
+			schemaPath:  "./test_data/image_parent.json",
+			want: map[string]Instance{
+				"$.type": {Type: "string"},
+				"$.crops": {Type: "array", Items: []byte(`{
+			        "type": "object",
+			        "properties": {
+			          "name": {
+			            "type": "string",
+			            "default": "name"
+			          },
+			          "width": {
+			            "type": "number"
+			          },
+			          "height": {
+			            "type": "number"
+			          },
+			          "path": {
+			            "type": "string"
+			          },
+			          "relativePath": {
+			            "type": "string"
+			          }
+			        },
+			        "required":[
+			          "name",
+			          "width",
+			          "height",
+			          "path",
+			          "relativePath"
+			        ]
+			    }`)},
+				"$.crops[*]": {Type: "object", Properties: map[string]json.RawMessage{
+					"name": []byte(`{
+			            "type": "string",
+			            "default": "name"
+			          }`),
+					"width": []byte(`{
+			            "type": "number"
+			          }`),
+					"height": []byte(`{
+			            "type": "number"
+			          }`),
+					"path": []byte(`{
+			            "type": "string"
+			          }`),
+					"relativePath": []byte(`{
+			            "type": "string"
+			          }`),
+				},
+					Required: []string{"name", "width", "height", "path", "relativePath"},
+				},
+				"$.crops[*].name":         {Type: "string"},
+				"$.crops[*].width":        {Type: "number"},
+				"$.crops[*].height":       {Type: "number"},
+				"$.crops[*].path":         {Type: "string"},
+				"$.crops[*].relativePath": {Type: "string"},
+				"$.URL": {Type: "object", Properties: map[string]json.RawMessage{
+					"publish": []byte(`{
+			          "type": "string",
+			          "transform": {
+			            "cumulo": {
+			              "from" : [
+			                {
+			                  "jsonPath": "$.publishUrl"
+			                }
+			              ]
+			            }
+			          }
+			        }`),
+					"absolute": []byte(`{
+			          "type": "string",
+			          "transform": {
+			            "cumulo": {
+			              "from" : [
+			                {
+			                  "jsonPath": "$.absoluteUrl"
+			                }
+			              ]
+			            }
+			          }
+			      }`)},
+					Required: []string{"publish", "absolute"},
+				},
+				"$.URL.publish":  {Type: "string"},
+				"$.URL.absolute": {Type: "string"},
+			},
+		},
+		{
+			description: "Advanced walk does it all",
+			oneOfType:   "array-of-array",
+			schemaPath:  "./test_data/parent.json",
+			want: map[string]Instance{
+				"$.type": {Type: "string"},
+				"$.crops": {Type: "array", Items: []byte(`{
+			        "type": "array",
+			        "items": {
+			          "type": "object",
+			          "properties": {
+			            "name": {
+			              "type": "string"
+			            }
+			          }
+			        }
+			    }`)},
+				"$.crops[*]": {Type: "array", Items: []byte(`{
+			          "type": "object",
+			          "properties": {
+			            "name": {
+			              "type": "string"
+			            }
+			          }
+			      }`)},
+				"$.crops[*][*]": {Type: "object", Properties: map[string]json.RawMessage{
+					"name": []byte(`{
+			              "type": "string"
+			        }`)},
+				},
+				"$.crops[*][*].name": {Type: "string"},
+			},
+		},
+		{
+			description: "Object with missing properties",
+			schemaPath:  "./test_data/bad-object.json",
+			wantErr:     true,
+		},
+		{
+			description: "Array with missing Items",
+			schemaPath:  "./test_data/bad-array.json",
+			wantErr:     true,
+		},
+	}
+
+	for _, test := range tests {
+		walker := testWalker{calls: make(map[string]Instance), rawCalls: make(map[string]json.RawMessage)}
+		schema, err := SchemaFromFile(test.schemaPath, test.oneOfType)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = Walk(schema, walker.walkFn)
+
+		switch {
+		case test.wantErr && err != nil:
+			continue
+		case test.wantErr && err == nil:
+			t.Errorf("Test %q - got nil error want error", test.description)
+		case !test.wantErr && err != nil:
+			t.Errorf("Test %q - got error: %v", test.description, err)
+			continue
+		}
+		if got, want := len(walker.calls), len(test.want); got != want {
+			t.Errorf("Test %q - got %d calls, want %d", test.description, got, want)
+		}
+		for key, call := range walker.calls {
+			got, err := json.Marshal(call)
+			if err != nil {
+				t.Fatal(err)
+			}
+			want, err := json.Marshal(test.want[key])
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("Test %q - at got key %q got call\n%s\n\twant\n%s", test.description, key, call, want)
+			}
+		}
+		for key, call := range test.want {
+			got, err := json.Marshal(walker.calls[key])
+			if err != nil {
+				t.Fatal(err)
+			}
+			want, err := json.Marshal(call)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("Test %q - at want key %q got call\n%s\n\twant\n%s", test.description, key, got, want)
+			}
+		}
+	}
+}
+func TestWalkJSONSchemaRaw(t *testing.T) {
 	tests := []struct {
 		description string
 		oneOfType   string
@@ -385,7 +659,7 @@ func TestWalkJSONSchema(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		walker := testWalker{calls: make(map[string]json.RawMessage)}
+		walker := testWalker{calls: make(map[string]Instance), rawCalls: make(map[string]json.RawMessage)}
 		schema, err := SchemaFromFile(test.schemaPath, test.oneOfType)
 		if err != nil {
 			t.Fatal(err)
@@ -401,17 +675,17 @@ func TestWalkJSONSchema(t *testing.T) {
 			t.Errorf("Test %q - got error: %v", test.description, err)
 			continue
 		}
-		if got, want := len(walker.calls), len(test.want); got != want {
+		if got, want := len(walker.rawCalls), len(test.want); got != want {
 			t.Errorf("Test %q - got %d calls, want %d", test.description, got, want)
 		}
-		for key, call := range walker.calls {
+		for key, call := range walker.rawCalls {
 			if !reflect.DeepEqual(call, test.want[key]) {
 				t.Errorf("Test %q - at got key %q got call\n%s\n\twant\n%s", test.description, key, call, test.want[key])
 			}
 		}
 		for key, call := range test.want {
-			if !reflect.DeepEqual(call, walker.calls[key]) {
-				t.Errorf("Test %q - at want key %q got call\n%s\n\twant\n%s", test.description, key, walker.calls[key], call)
+			if !reflect.DeepEqual(call, walker.rawCalls[key]) {
+				t.Errorf("Test %q - at want key %q got call\n%s\n\twant\n%s", test.description, key, walker.rawCalls[key], call)
 			}
 		}
 	}
