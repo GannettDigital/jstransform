7317eb52deba04e6b65cb6606093eeaec4a19ae5
ikenticus <ikenticus@gmail.com>
2018-04-05 11:29:09 -0400

    PENG-1831 updates based on Tim review

--- main.go
+++ main.go
@@ -12,19 +12,26 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Printf("Usage: %s <JSON Schema Path> [output directory]\n", path.Base(os.Args[0]))
-		os.Exit(0)
+		os.Exit(1)
+	}
+
+	inputPath, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		fmt.Printf("Input directory %s error: %v", os.Args[1], err)
+		os.Exit(2)
 	}
 
 	var outputPath string
 	if len(os.Args) == 3 {
-		outputPath, _ = filepath.Abs(os.Args[2])
+		outputPath, err = filepath.Abs(os.Args[2])
+		if err != nil {
+			fmt.Printf("Output directory %s error: %v", os.Args[2], err)
+			os.Exit(3)
+		}
 	}
 
-	inputPath, _ := filepath.Abs(os.Args[1])
-	err := generate.BuildStructs(inputPath, outputPath)
-	if err == nil {
-		fmt.Printf("Finished, check output directory %s\n", outputPath)
-	} else {
-		panic(err)
+	if err = generate.BuildStructs(inputPath, outputPath); err != nil {
+		fmt.Printf("Golang Struct generation failed: %v", err)
+		os.Exit(4)
 	}
 }
