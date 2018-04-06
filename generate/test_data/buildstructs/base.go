d97637e263db5c66d1bcc7732088b1f5504075f6
jlin <jlin@usatoday.com>
2018-04-06 11:38:32 -0400

    PENG-1839 revert to pre jstransform

--- generate/test_data/buildstructs/base.go
+++ generate/test_data/buildstructs/base.go
@@ -6,26 +6,28 @@ type Base struct {
 	AdsEnabled        bool   `json:"adsEnabled"`
 	AssetDocumentData string `json:"assetDocumentData"`
 	AssetGroup        struct {
-		Id       string `json:"id"`
-		LogoURL  string `json:"logoURL,omitempty"`
-		Name     string `json:"name"`
-		SiteCode string `json:"siteCode,omitempty"`
-		SiteId   string `json:"siteId"`
-		SiteName string `json:"siteName"`
-		SstsId   string `json:"sstsId,omitempty"`
-		Type     string `json:"type,omitempty"`
-		URL      string `json:"URL,omitempty"`
+		ID         string  `json:"id"`
+		LogoURL    string  `json:"logoURL"`
+		Name       string  `json:"name"`
+		PropertyID float64 `json:"propertyId"`
+		SiteCode   string  `json:"siteCode"`
+		SiteID     string  `json:"siteId"`
+		SiteName   string  `json:"siteName"`
+		SstsID     string  `json:"sstsId"`
+		Title      string  `json:"title"`
+		Type       string  `json:"type"`
+		URL        string  `json:"URL"`
 	} `json:"assetGroup"`
 	AuthoringBehavior      string `json:"authoringBehavior"`
 	AuthoringTypeCode      string `json:"authoringTypeCode"`
 	AwsPath                string `json:"awsPath"`
-	BackfillDate           string `json:"backfillDate,omitempty"`
-	BookReviewPageURL      string `json:"bookReviewPageURL,omitempty"`
-	Byline                 string `json:"byline,omitempty"`
+	BackfillDate           string `json:"backfillDate"`
+	BookReviewPageURL      string `json:"bookReviewPageURL"`
+	Byline                 string `json:"byline"`
 	ContentProtectionState string `json:"contentProtectionState"`
-	ContentSourceCode      string `json:"contentSourceCode,omitempty"`
+	ContentSourceCode      string `json:"contentSourceCode"`
 	Contributors           []struct {
-		Id   string `json:"id"`
+		ID   string `json:"id"`
 		Name string `json:"name"`
 	} `json:"contributors"`
 	CreateDate        string `json:"createDate"`
@@ -34,71 +36,72 @@ type Base struct {
 	EmbargoDate       string `json:"embargoDate"`
 	EventDate         string `json:"eventDate"`
 	ExcludeFromMobile bool   `json:"excludeFromMobile"`
-	ExpirationDate    string `json:"expirationDate,omitempty"`
+	ExpirationDate    string `json:"expirationDate"`
 	Fronts            []struct {
-		Id              string `json:"id"`
+		ID              string `json:"id"`
 		Name            string `json:"name"`
-		RecommendedDate string `json:"recommendedDate"`
 		Type            string `json:"type"`
+		RecommendedDate string `json:"recommendedDate"`
 	} `json:"fronts"`
-	Headline           string `json:"headline,omitempty"`
-	Id                 string `json:"id"`
-	InitialPublishDate string `json:"initialPublishDate,omitempty"`
+	Headline           string `json:"headline"`
+	ID                 string `json:"id"`
+	InitialPublishDate string `json:"initialPublishDate"`
 	IsEvergreen        bool   `json:"isEvergreen"`
-	Keywords           string `json:"keywords,omitempty"`
+	Keywords           string `json:"keywords"`
 	Links              struct {
 		Assets []struct {
-			Id        string `json:"id"`
+			ID        string `json:"id"`
 			Overrides struct {
-				Caption string `json:"caption,omitempty"`
-			} `json:"overrides,omitempty"`
+				Caption string `json:"caption"`
+			} `json:"overrides"`
 			Position              float64 `json:"position"`
 			RelationshipTypeFlags string  `json:"relationshipTypeFlags"`
 		} `json:"assets"`
-		PhotoId string `json:"photoId,omitempty"`
+		PhotoID string `json:"photoId"`
 	} `json:"links"`
 	PageURL struct {
 		Long  string `json:"long"`
 		Short string `json:"short"`
 	} `json:"pageURL"`
-	PromoBrief            string  `json:"promoBrief,omitempty"`
-	PropertyDisplayName   string  `json:"propertyDisplayName"`
-	PropertyId            string  `json:"propertyId"`
-	PropertyName          string  `json:"propertyName"`
-	Publication           string  `json:"publication"`
-	PublishDate           string  `json:"publishDate"`
-	PublishSystem         string  `json:"publishSystem,omitempty"`
-	PublishUser           string  `json:"publishUser,omitempty"`
-	ReaderCommentsEnabled bool    `json:"readerCommentsEnabled"`
-	SchemaVersion         float64 `json:"schemaVersion"`
-	ShortHeadline         string  `json:"shortHeadline"`
-	Source                string  `json:"source,omitempty"`
+	PromoBrief            string `json:"promoBrief"`
+	PropertyID            string `json:"propertyId"`
+	PropertyName          string `json:"propertyName"`
+	PropertyDisplayName   string `json:"propertyDisplayName"`
+	Publication           string `json:"publication"`
+	PublishDate           string `json:"publishDate"`
+	PublishSystem         string `json:"publishSystem"`
+	PublishUser           string `json:"publishUser"`
+	ReaderCommentsEnabled bool   `json:"readerCommentsEnabled"`
+	SchemaVersion         int    `json:"schemaVersion"`
+	ShortHeadline         string `json:"shortHeadline"`
+	Source                string `json:"source"`
 	Ssts                  struct {
-		LeafName                  string `json:"leafName,omitempty"`
-		Path                      string `json:"path,omitempty"`
-		Section                   string `json:"section,omitempty"`
-		Storysubject              string `json:"storysubject,omitempty"`
-		Subsection                string `json:"subsection,omitempty"`
-		Subtopic                  string `json:"subtopic,omitempty"`
-		TaxonomyEntityDisplayName string `json:"taxonomyEntityDisplayName,omitempty"`
-		Topic                     string `json:"topic,omitempty"`
-	} `json:"ssts,omitempty"`
+		LeafName                  string `json:"leafName"`
+		Path                      string `json:"path"`
+		Section                   string `json:"section"`
+		Storysubject              string `json:"storysubject"`
+		Subsection                string `json:"subsection"`
+		Subtopic                  string `json:"subtopic"`
+		TaxonomyEntityDisplayName string `json:"taxonomyEntityDisplayName"`
+		Topic                     string `json:"topic"`
+	} `json:"ssts"`
 	StatusName      string   `json:"statusName"`
 	StoryHighlights []string `json:"storyHighlights"`
 	Tags            []struct {
 		DateTagged     string  `json:"dateTagged"`
-		Id             string  `json:"id"`
+		ID             string  `json:"id"`
 		IsPrimary      bool    `json:"isPrimary"`
 		Name           string  `json:"name"`
-		ParentId       string  `json:"parentId,omitempty"`
+		ParentID       string  `json:"parentId"`
 		Path           string  `json:"path"`
 		RelevanceScore float64 `json:"relevanceScore"`
 		TaggingStatus  string  `json:"taggingStatus"`
 		TopicType      string  `json:"topicType"`
 		Type           string  `json:"type"`
-	} `json:"tags,omitempty"`
+	} `json:"tags"`
 	Title      string `json:"title"`
 	Topic      string `json:"topic"`
+	Type       string `json:"type"`
 	UpdateDate string `json:"updateDate"`
 	UpdateUser string `json:"updateUser"`
 }
