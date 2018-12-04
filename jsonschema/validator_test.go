d840ba0b8927bb0a0305e3f1a6e2a57ed4e88e09
abaehre <abaehre@gannett.com>
2018-12-04 10:06:45 -0500

    Small tweak to error message

--- jsonschema/validator_test.go
+++ jsonschema/validator_test.go
@@ -53,10 +53,10 @@ func TestValidate(t *testing.T) {
 		got, err := v.Validate(raw)
 		if err != nil && test.wantErr != nil {
 			if err.Error() != test.wantErr.Error() {
-				t.Errorf("Test %q - got error %v, want error %t", test.description, err, test.wantErr)
+				t.Errorf("Test %q - got error %s, want error %s", test.description, err.Error(), test.wantErr.Error())
 			}
 		} else if err != nil {
-			t.Errorf("Test %q - got error %v, want error %t", test.description, err, test.wantErr)
+			t.Errorf("Test %q - got error %s, want error %s", test.description, err.Error(), test.wantErr)
 		}
 		if got != test.wantValid {
 			t.Errorf("Test %q - got valid %t, want %t", test.description, got, test.wantValid)
