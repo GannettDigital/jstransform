3bd51fc8afbfc0bbc8f552e11d5d32a1e9343751
Kristi Vincent <vincentkb@live.com>
2019-06-29 00:10:43 -0600

    looking at parse results

--- transform/transformer_test.go
+++ transform/transformer_test.go
@@ -485,7 +485,7 @@ func TestCurrentTimeTransform(t *testing.T) {
 				wantParse := strings.Split(string(wantResult),`"`)[3]
 
 				gotResult, err := json.Marshal(got)
-				fmt.Printf("gotResult : %v",strings.Split(string(gotResult), `"'`)[1])
+				fmt.Printf("gotResult : %v",strings.Split(string(gotResult), `"'`)[0])
 				if err != nil {
 					t.Fatalf("unable to marshal got. got: %v", got)
 				}
