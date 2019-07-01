2aea29eca4f419f020754c58abff0088357cdda6
Andy Horner <ahornerr@gmail.com>
2019-07-01 10:24:58 -0600

    Changed dereferencing logic so that only a `transform` object will get applied after a ref's contents are set. This gets us closer in line with JSONSchema spec.

--- jsonschema/reference_test.go
+++ jsonschema/reference_test.go
@@ -76,6 +76,10 @@ func TestDereference(t *testing.T) {
 			description: "Sports Matchup Example",
 			schemaPath:  "./test_data/reference/jsref_matchup-mlb.json",
 		},
+		{
+			description: "Keep Source Transform when Referencing",
+			schemaPath:  "./test_data/reference/jsref_keep-transform.json",
+		},
 	}
 	for _, test := range tests {
 
@@ -105,6 +109,7 @@ func TestDereference(t *testing.T) {
 		case test.wantErr && err != nil:
 			continue
 		case !reflect.DeepEqual(got, want):
+			t.Logf("%s", gotJson)
 			t.Errorf("Test %q - got\n%s\nwant\n%s", test.description, got, want)
 		}
 	}
